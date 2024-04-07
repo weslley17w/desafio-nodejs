24a5752ef4292d9b3b9c2e917c546d038f2c6ab0
Weslley Barros <weslley17w@gmail.com>
2024-04-07 16:15:22 -0300

    chore: Adiciona setup para teste unitários

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  clearMocks: true,
+  coverageProvider: 'v8',
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/**/*.spec.ts']
+};
